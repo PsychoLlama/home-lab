4060dc17126313fe795bb278cf445c690210ec86
Jesse Gibson <JesseTheGibson@gmail.com>
2021-07-17 11:49:13 -0700

    Enable SSH on host machines
    The only authorized user is my own public key. Passwords are disabled
    because duh.

--- nixos/lib.nix
+++ nixos/lib.nix
@@ -9,12 +9,21 @@ inputs:
     };
 
     modules = [
-      ({ lib, ... }: {
+      ({ lib, pkgs, ... }: {
         # Match the directory name to the host's name.
         networking.hostName = lib.mkDefault (baseNameOf path);
 
         # Attach the git sha to `nixos-version` output.
         system.configurationRevision = inputs.self.rev or null;
+
+        services.openssh = {
+          enable = true;
+          passwordAuthentication = false;
+        };
+
+        users.users.root.openssh.authorizedKeys.keys = [
+          (builtins.readFile ./ssh-key.pub)
+        ];
       })
 
       path
