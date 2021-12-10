78e6fdd11267837dcda5d904e3d47b1c848ba5fb
Jesse Gibson <JesseTheGibson@gmail.com>
2021-12-10 15:42:55 -0800

    Remove deploy responsibility from Multivac
    I'm deploying from my development laptop now. See: a3213a9

--- machines/hosts/multivac/README.md
+++
@@ -1,5 +0,0 @@
-# Cluster Manager
-
-This machine is responsible for managing the cluster with NixOps, including itself. The `home-lab` repository is edited and deployed from here.
-
-To make changes, mount the directory on a remote dev machine using `sshfs` and call `nixops` remotely.
--- machines/hosts/multivac/default.nix
+++ machines/hosts/multivac/default.nix
@@ -2,36 +2,5 @@
 
 {
   imports = [ ./hardware-configuration.nix ];
-
-  nix = {
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        hostName = "tron";
-        sshUser = "root";
-        system = "aarch64-linux";
-      }
-      {
-        hostName = "clu";
-        sshUser = "root";
-        system = "aarch64-linux";
-      }
-    ];
-  };
-
-  # BOOTSTRAP PROCESS: Initialize this machine then copy the key to every
-  # worker node.
-  #
-  #   nixops deploy --include cluster-manager
-  #   ssh-copy-id root@<worker>
-  #
-  services.openssh.hostKeys = [{
-    type = "ed25519";
-    path = "/root/.ssh/id_ed25519";
-    comment = "NixOps deploy key";
-  }];
-
-  environment.systemPackages = with pkgs; [ nixopsUnstable neovim ];
-
   system.stateVersion = "21.05";
 }
