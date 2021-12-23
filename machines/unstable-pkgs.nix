055466d17c9609f05042a5717bb65cb713d7c665
Jesse Gibson <JesseTheGibson@gmail.com>
2021-12-22 20:46:43 -0800

    Add ability to run all inf tests through nix flake
    `checks` is a standardized flake field for the set of derivations to
    build for testing. Use `nix flake check --keep-going` to run all tests.

--- machines/unstable-pkgs.nix
+++ machines/unstable-pkgs.nix
@@ -1,5 +1,8 @@
-let
-  nixpkgs =
-    fetchTarball "https://github.com/NixOS/nixpkgs/archive/f225322e3b.tar.gz";
-
-in import nixpkgs # Note: still expects a config argument.
+# NixOps doesn't allow custom arguments in NixOS configurations. Unstable
+# packages cannot reasonably be passed through the Flake.
+#
+# Note: the result still expects a config argument.
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/f225322e3b.tar.gz";
+  sha256 = "1cbl7w81h2m4as15z094jkcrgg2mdi2wnkzg2dhd6080vgic11vy";
+})
