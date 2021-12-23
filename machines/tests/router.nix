4239dddfe4a5458a54c09a9bcc0577624db76fb9
Jesse Gibson <JesseTheGibson@gmail.com>
2021-12-22 19:42:32 -0800

    Add service for running a router on NixOS
    I've been using OPNsense for a while as my home router, but the
    configuration is all mouse driven and much of the product is just
    a complex UI over features that are already built into Linux. I'd much
    rather manage it declaratively the same as the rest of my
    infrastructure, then I can leverage the full power of the dark sid...
    er, NixOS.
    
    Right now I'm able to access my router at `selfhosted.city`, but only
    through the LAN interface. I need to move control of that domain to the
    new private ingress server, and ideally run ACME near a Vault service
    instead. The goal is to free up the domain and enable tighter network
    controls, like VLANs, before opening anything up to the internet.

---
+++ machines/tests/router.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import ../unstable-pkgs.nix { } }:
+
+{
+  basic = pkgs.nixosTest {
+    name = "router-basic";
+    nodes = {
+      router = {
+        imports = [ ../services/router.nix ];
+        virtualisation.vlans = [ 1 2 ];
+        networking.interfaces.eth1.useDHCP = false;
+
+        lab.router = {
+          enable = true;
+          network.wan.interface = "eth1";
+          network.lan.interface = "eth2";
+        };
+      };
+
+      client = {
+        virtualisation.vlans = [ 2 ];
+        networking = {
+          useDHCP = false;
+          interfaces.eth1.useDHCP = true;
+        };
+      };
+    };
+
+    testScript = ''
+      start_all()
+
+      client.wait_for_unit('network-online.target')
+      client.succeed('ip addr show eth1 to 192.168.1.1/24 | grep inet')
+    '';
+  };
+}
