3233ac5c86df459afbe1f9429348560daa5d5169
Jesse Gibson <JesseTheGibson@gmail.com>
2022-01-03 18:30:33 -0800

    Discover Consul/Nomad server peers through DNS
    Now that I've got service records for Consul and Nomad, they're much
    more easily configured. I don't need to enumerate all the nodes and
    extract host addresses anymore, I just point them to the DNS record.

--- machines/services/consul.nix
+++ machines/services/consul.nix
@@ -1,24 +1,13 @@
 { nodes, config, lib, pkgs, ... }:
 
-# Consul
-#
-# Configures HashiCorp Consul and automatically federates with all other
-# machines that enable the service mesh.
-
 with lib;
 
 let
   unstable = import ../unstable-pkgs.nix { system = pkgs.system; };
   cfg = config.lab.consul;
-  myAddress = config.networking.fqdn;
 
-  otherConsulServers = mapAttrsToList (_: node: node.config.networking.fqdn)
-    (filterAttrs (_: node:
-      node.config.lab.consul.server.enable && node.config.networking.fqdn
-      != myAddress) nodes);
-
-  expectedServerCount = length otherConsulServers
-    + (if cfg.server.enable then 1 else 0);
+  expectedServerCount = length (attrValues
+    (filterAttrs (_: node: node.config.lab.consul.server.enable) nodes));
 
 in {
   options.lab.consul = {
@@ -51,56 +40,27 @@ in {
       });
     };
 
-    networking.firewall.allowedTCPPortRanges = [
-      # DNS
-      {
-        from = 8600;
-        to = 8600;
-      }
+    networking.firewall = {
+      allowedTCPPorts = [
+        8600 # DNS
+        8500 # HTTP API
+        8502 # gRPC API
+        8300 # Server-to-server RPC
+        8301 # LAN Serf
+        8302 # WAN Serf
+      ];
 
-      # HTTP API
-      {
-        from = 8500;
-        to = 8500;
-      }
-
-      # gRPC API
-      {
-        from = 8502;
-        to = 8502;
-      }
-
-      # Server-to-server RPC
-      {
-        from = 8300;
-        to = 8300;
-      }
-
-      # LAN/WAN Serf
-      {
-        from = 8301;
-        to = 8302;
-      }
+      allowedUDPPorts = [
+        8600 # DNS
+        8301 # LAN Serf
+        8302 # WAN Serf
+      ];
 
       # Sidecar proxy
-      {
+      allowedTCPPortRanges = [{
         from = 21000;
         to = 21255;
-      }
-    ];
-
-    networking.firewall.allowedUDPPortRanges = [
-      # DNS
-      {
-        from = 8600;
-        to = 8600;
-      }
-
-      # LAN/WAN Serf
-      {
-        from = 8301;
-        to = 8302;
-      }
-    ];
+      }];
+    };
   };
 }
--- machines/services/nomad.nix
+++ machines/services/nomad.nix
@@ -1,18 +1,13 @@
 { nodes, config, lib, pkgs, ... }:
 
-# container-orchestration
-#
-# Configures HashiCorp Nomad in server mode, automatically set to federate
-# with other nomad instances discovered over Consul.
-
 with lib;
 
 let
   unstable = import ../unstable-pkgs.nix { system = pkgs.system; };
   cfg = config.lab.nomad;
-  nomadServers = attrValues
-    (filterAttrs (_: node: with node.config.lab.nomad; enable && server.enable)
-      nodes);
+
+  expectedServerCount = length (attrValues
+    (filterAttrs (_: node: node.config.lab.nomad.server.enable) nodes));
 
 in {
   options.lab.nomad = {
@@ -25,8 +20,8 @@ in {
     };
   };
 
-  config = {
-    services.nomad = mkIf cfg.enable {
+  config = mkIf cfg.enable {
+    services.nomad = {
       enable = true;
       dropPrivileges = false;
       package = unstable.nomad;
@@ -37,15 +32,13 @@ in {
       settings = {
         server = {
           enabled = cfg.server.enable;
-          bootstrap_expect = length nomadServers;
+          bootstrap_expect = expectedServerCount;
         };
 
         client = {
           enabled = cfg.client.enable;
           cni_path = "${unstable.cni-plugins}/bin";
-
-          servers =
-            forEach nomadServers (server: server.config.networking.fqdn);
+          servers = [ "nomad.service.selfhosted.city" ];
 
           # Force downgrade Envoy. See:
           # https://github.com/envoyproxy/envoy/issues/15235
@@ -59,44 +52,28 @@ in {
       };
     };
 
-    networking.firewall.allowedTCPPortRanges = mkIf cfg.enable [
-      # HTTP API
-      {
-        from = 4646;
-        to = 4646;
-      }
+    networking.firewall = {
+      allowedTCPPorts = [
+        4646 # HTTP API
+        4647 # Private RPC
+        4648 # Serf WAN
+      ];
 
-      # Private RPC
-      {
-        from = 4647;
-        to = 4647;
-      }
-
-      # Serf WAN
-      {
-        from = 4648;
-        to = 4648;
-      }
+      allowedUDPPorts = [
+        4648 # Serf WAN
+      ];
 
       # Dynamic port allocations
-      {
+      allowedTCPPortRanges = [{
         from = 20000;
         to = 32000;
-      }
-    ];
-
-    networking.firewall.allowedUDPPortRanges = mkIf cfg.enable [
-      # Serf WAN
-      {
-        from = 4648;
-        to = 4648;
-      }
+      }];
 
       # Dynamic port allocations
-      {
+      allowedUDPPortRanges = [{
         from = 20000;
         to = 32000;
-      }
-    ];
+      }];
+    };
   };
 }
