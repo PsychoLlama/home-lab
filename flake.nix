0d90944dd34f00efb1cd66f90cd6e6b2bae2c5b6
Jesse Gibson <JesseTheGibson@gmail.com>
2023-04-25 23:37:31 -0400

    Remove flake-utils input
    I've found a better way through per-project flake files: I'm using
    `genAttrs systems.flakeExposed`, which is functionally identical, but
    doesn't incur a dependency (or weird attrset joins).

--- flake.nix
+++ flake.nix
@@ -4,25 +4,22 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/22.11";
     hardware.url = "github:NixOS/nixos-hardware";
-    flake-utils.url = "github:numtide/flake-utils";
     dns-blocklist = {
       url = "github:StevenBlack/hosts";
       flake = false;
     };
   };
 
-  outputs = inputs@{ self, nixpkgs-unstable, nixpkgs, hardware, flake-utils
-    , dns-blocklist }:
+  outputs = inputs@{ self, nixpkgs-unstable, nixpkgs, hardware, dns-blocklist }:
+    with nixpkgs.lib;
 
     let
       defineHost = import ./machines/define-host.nix;
-      hostDefinitions = with nixpkgs.lib;
-        (mapAttrs (hostName: _: defineHost hostName)
-          (filterAttrs (_: pathType: pathType == "directory")
-            (builtins.readDir ./machines/hosts)));
+      hostDefinitions = (mapAttrs (hostName: _: defineHost hostName)
+        (filterAttrs (_: pathType: pathType == "directory")
+          (builtins.readDir ./machines/hosts)));
 
       pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
-      unstable = import inputs.nixpkgs-unstable { system = pkgs.system; };
 
     in {
       colmena = hostDefinitions // {
@@ -44,41 +41,41 @@
           # remote builders.
         };
       };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let unstable = import nixpkgs-unstable { inherit system; };
 
-      in {
-        checks = import ./tests { pkgs = unstable; };
+      checks = genAttrs systems.flakeExposed (system:
+        import ./tests { pkgs = import nixpkgs-unstable { inherit system; }; });
 
-        devShell = with unstable;
-          mkShell {
-            nativeBuildInputs = [
-              (callPackage ./machines/pkgs/vault-client.nix { })
-              vault
-              nixUnstable
-              colmena
-            ];
+      devShell = genAttrs systems.flakeExposed (system:
+        with import nixpkgs-unstable { inherit system; };
 
-            # NOTE: Configuring remote builds through the client assumes you
-            # are a trusted Nix user. Without permission, you'll see errors
-            # where it refuses to compile a foreign architecture.
-            NIX_CONFIG = ''
-              experimental-features = nix-command flakes
-              builders-use-substitutes = true
-              builders = @${
-                writeText "nix-remote-builders" ''
-                  ssh://root@glados.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
-                  ssh://root@tron.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
-                  ssh://root@clu.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
-                ''
-              }
-            '';
+        mkShell {
+          nativeBuildInputs = [
+            (callPackage ./machines/pkgs/vault-client.nix { })
+            vault
+            nixUnstable
+            colmena
+          ];
 
-            shellHook = ''
-              if [[ -z "$VAULT_TOKEN" ]]; then
-                echo '--- Remember to set $VAULT_TOKEN ---' >&2
-              fi
-            '';
-          };
-      });
+          # NOTE: Configuring remote builds through the client assumes you
+          # are a trusted Nix user. Without permission, you'll see errors
+          # where it refuses to compile a foreign architecture.
+          NIX_CONFIG = ''
+            experimental-features = nix-command flakes
+            builders-use-substitutes = true
+            builders = @${
+              writeText "nix-remote-builders" ''
+                ssh://root@glados.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
+                ssh://root@tron.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
+                ssh://root@clu.host.selfhosted.city aarch64-linux /root/.ssh/home_lab 4
+              ''
+            }
+          '';
+
+          shellHook = ''
+            if [[ -z "$VAULT_TOKEN" ]]; then
+              echo '--- Remember to set $VAULT_TOKEN ---' >&2
+            fi
+          '';
+        });
+    };
 }
