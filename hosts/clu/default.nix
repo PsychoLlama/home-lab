63f37231cbd80f29f19e2206e2c5a3f043233fd5
Jesse Gibson <JesseTheGibson@gmail.com>
2023-10-29 04:08:58 -0400

    Move router to rpi4
    A few things have made me wary of continuing essential services on the
    RPi3 devices:
    
    - They don't have enough RAM to build a nixos image. If I mess up vlans
      (again), I won't be able to deploy changes to my router.
    - It's getting increasingly difficult to upgrade my NixOS distros. RPi4
      upgrades without a hitch. RPi3 is always on the brink of annihilation.
      I just bricked my file server again.
    
    I'd rather waste the resources of a raspi 4 and be able to continue
    maintaining the system than risk getting locked out or stuck on an old
    version.
    
    This commit migrates the router service to one of my idle raspi
    4 devices.

--- hosts/clu/default.nix
+++ hosts/clu/default.nix
@@ -1,9 +1,194 @@
-{
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  mdns-interfaces = [ "vlan-iot" "wap" "vlan-guest" ];
+  mdns-ports = [ 5353 ];
+
+  xbox-ip-address = "10.0.2.250";
+  xbox-live-ports = {
+    tcp = [ 3074 ];
+    udp = [ 3074 3075 88 500 3544 4500 ];
+  };
+
+in {
   imports = [ ../../modules/hardware/raspberry-pi-4.nix ];
 
-  lab.network = {
-    ethernetAddress = "dc:a6:32:e1:42:81";
-    ipAddress = "10.0.0.203";
+  # Assign sensible names to the network interfaces. Anything with vlans needs
+  # a hardware-related filter to avoid conflicts with virtual devices.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="net", ENV{ID_BUS}=="usb", ATTR{address}=="b0:a7:b9:2c:a9:b5", NAME="wap"
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="60:a4:b7:59:07:f2", NAME="wan"
+    ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="dc:a6:32:e1:42:81", NAME="lan"
+  '';
+
+  # VLANs are sent by the WAP (a UniFi U6 Lite).
+  networking.vlans = {
+    vlan-iot = {
+      id = 10;
+      interface = "wap";
+    };
+
+    vlan-work = {
+      id = 20;
+      interface = "wap";
+    };
+
+    vlan-guest = {
+      id = 30;
+      interface = "wap";
+    };
+  };
+
+  # Bridge mDNS between my IoT, Guest, and private LAN.
+  services.avahi = {
+    enable = true;
+    reflector = true;
+    openFirewall = false;
+    nssmdns = true;
+    interfaces = mdns-interfaces;
+  };
+
+  lab = {
+    network = {
+      ethernetAddress = "dc:a6:32:e1:42:81";
+      ipAddress = "10.0.0.1";
+    };
+
+    router = {
+      enable = true;
+      debugging.enable = true;
+
+      dns = {
+        blocklist = "${pkgs.unstable.stevenblack-blocklist}/hosts";
+        records = [ ];
+      };
+
+      network = {
+        wan.interface = "wan"; # Dongle to WAN
+
+        subnets = [
+          {
+            mask = "255.255.255.0";
+            bits = 24;
+            start = "10.0.0.0";
+            broadcast = "10.0.0.255";
+
+            lease = {
+              start = "10.0.0.10";
+              end = "10.0.0.200";
+            };
+
+            link = {
+              interface = "lan"; # Dongle to ethernet switch
+              address = "10.0.0.1";
+            };
+          }
+          {
+            mask = "255.255.255.0";
+            bits = 24;
+            start = "10.0.1.0";
+            broadcast = "10.0.1.255";
+
+            lease = {
+              start = "10.0.1.10";
+              end = "10.0.1.250";
+            };
+
+            link = {
+              interface = "wap"; # Dongle to WAP (no VLAN)
+              address = "10.0.1.1";
+            };
+          }
+          {
+            mask = "255.255.255.0";
+            bits = 24;
+            start = "10.0.2.0";
+            broadcast = "10.0.2.255";
+
+            lease = {
+              start = "10.0.2.10";
+              end = "10.0.2.250";
+            };
+
+            link = {
+              interface = "vlan-iot";
+              address = "10.0.2.1";
+            };
+          }
+          {
+            mask = "255.255.255.0";
+            bits = 24;
+            start = "10.0.3.0";
+            broadcast = "10.0.3.255";
+
+            lease = {
+              start = "10.0.3.10";
+              end = "10.0.3.250";
+            };
+
+            link = {
+              interface = "vlan-work";
+              address = "10.0.3.1";
+            };
+          }
+          {
+            mask = "255.255.255.0";
+            bits = 24;
+            start = "10.0.4.0";
+            broadcast = "10.0.4.255";
+
+            lease = {
+              start = "10.0.4.10";
+              end = "10.0.4.250";
+            };
+
+            link = {
+              interface = "vlan-guest";
+              address = "10.0.4.1";
+            };
+          }
+        ];
+
+        extraHosts = [
+          {
+            ethernetAddress = "b0:60:88:19:d2:55";
+            ipAddress = "10.0.1.250";
+            hostName = "ava";
+          }
+          {
+            ethernetAddress = "20:16:42:06:2c:e3";
+            ipAddress = xbox-ip-address;
+            hostName = "xbox-console";
+          }
+        ];
+      };
+    };
+  };
+
+  # Although not technically part of the home lab, this is still my home
+  # router and some networking requirements are bound to bleed over.
+  #
+  # This opens ports for multiplayer gaming on Xbox Live.
+  networking = {
+    nat.forwardPorts = forEach xbox-live-ports.tcp (port: {
+      sourcePort = port;
+      destination = "${xbox-ip-address}:${builtins.toString port}";
+      proto = "tcp";
+    }) ++ forEach xbox-live-ports.udp (port: {
+      sourcePort = port;
+      destination = "${xbox-ip-address}:${builtins.toString port}";
+      proto = "udp";
+    });
+
+    firewall.interfaces.${config.lab.router.network.wan.interface} = {
+      allowedUDPPorts = xbox-live-ports.udp;
+      allowedTCPPorts = xbox-live-ports.tcp;
+    };
+
+    firewall.interfaces.vlan-iot.allowedUDPPorts = mdns-ports;
+    firewall.interfaces.wap.allowedUDPPorts = mdns-ports;
   };
 
   system.stateVersion = "21.05";
