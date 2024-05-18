1a3ed5661a8c243a86fd286b10aa47c14fecedf6
Jesse Gibson <JesseTheGibson@gmail.com>
2024-05-18 11:39:06 -0400

    Flatten extraneous test hierarchy
    All my test files only include a single test VM. I try to pack all
    subtests in a single run for speed. Flattening the hierarchy makes it
    nicer to run tests. I can just do `project test dhcp` and it works.
    I don't need to know the name of the subtest.
    
    Who reads these commit messages anyway.

--- nixos/tests/dhcp.nix
+++ nixos/tests/dhcp.nix
@@ -1,115 +1,113 @@
 { makeTest, lib, ... }:
 
-{
-  assignment = makeTest {
-    name = "assignment";
+makeTest {
+  name = "dhcp-assignment";
 
-    defaults.lab.networks.test.ipv4 = {
-      cidr = "10.0.5.3/24";
-      dhcp.pools = [
-        {
-          start = "10.0.5.22";
-          end = "10.0.5.22";
-        }
-      ];
-    };
+  defaults.lab.networks.test.ipv4 = {
+    cidr = "10.0.5.3/24";
+    dhcp.pools = [
+      {
+        start = "10.0.5.22";
+        end = "10.0.5.22";
+      }
+    ];
+  };
 
-    nodes = {
-      server =
-        { config, ... }:
-        {
-          virtualisation.vlans = [ 1 ];
-          networking.useDHCP = false;
+  nodes = {
+    server =
+      { config, ... }:
+      {
+        virtualisation.vlans = [ 1 ];
+        networking.useDHCP = false;
 
-          lab.services.dhcp = {
-            enable = true;
-            networks.test.interface = "eth1";
-            reservations = [
-              {
-                hw-address = "52:54:00:12:01:02";
-                ip-address = "10.0.5.68";
-              }
-            ];
-          };
-
-          assertions = [
+        lab.services.dhcp = {
+          enable = true;
+          networks.test.interface = "eth1";
+          reservations = [
             {
-              assertion = lib.any (port: port == 67) config.networking.firewall.interfaces.eth1.allowedUDPPorts;
-
-              message = ''
-                DHCP server did not open firewall.
-              '';
+              hw-address = "52:54:00:12:01:02";
+              ip-address = "10.0.5.68";
             }
           ];
+        };
 
-          systemd.network = {
-            enable = true;
-            networks = {
-              "01-eth1" = {
-                name = "eth1";
-                networkConfig.Address = "10.0.5.11/24";
-              };
+        assertions = [
+          {
+            assertion = lib.any (port: port == 67) config.networking.firewall.interfaces.eth1.allowedUDPPorts;
+
+            message = ''
+              DHCP server did not open firewall.
+            '';
+          }
+        ];
+
+        systemd.network = {
+          enable = true;
+          networks = {
+            "01-eth1" = {
+              name = "eth1";
+              networkConfig.Address = "10.0.5.11/24";
             };
           };
         };
-
-      client = {
-        virtualisation.vlans = [ 1 ];
-        systemd.services.systemd-networkd = {
-          environment.SYSTEMD_LOG_LEVEL = "debug";
-        };
-
-        networking = {
-          useNetworkd = true;
-          useDHCP = false;
-          interfaces.eth1.useDHCP = true;
-        };
       };
 
-      reserved = {
-        virtualisation.vlans = [ 1 ];
+    client = {
+      virtualisation.vlans = [ 1 ];
+      systemd.services.systemd-networkd = {
+        environment.SYSTEMD_LOG_LEVEL = "debug";
+      };
 
-        networking = {
-          useNetworkd = true;
-          useDHCP = false;
-          interfaces.eth1 = {
-            useDHCP = true;
-            macAddress = "52:54:00:12:01:02";
-          };
-        };
+      networking = {
+        useNetworkd = true;
+        useDHCP = false;
+        interfaces.eth1.useDHCP = true;
       };
     };
 
-    testScript = ''
-      import json
+    reserved = {
+      virtualisation.vlans = [ 1 ];
 
-      start_all()
-
-      server.wait_for_unit("kea-dhcp4-server.service")
-      client.wait_for_unit("systemd-networkd-wait-online.service")
-
-      with subtest("correct client IP is assigned"):
-        client.wait_until_succeeds("ip addr show eth1 | grep -q '10.0.5.22/24'")
-
-      with subtest("default gateway is assigned"):
-        routes = json.loads(client.succeed("ip --json route"))
-        gateways = {
-          route["gateway"] for route in routes
-          if (
-            route["dev"] == "eth1" and
-            route["protocol"] == "dhcp" and
-            "gateway" in route
-          )
-        }
-
-        assert "10.0.5.3" in gateways, f"Gateway was not assigned: {gateways}"
-
-      with subtest("expected DNS servers are provided"):
-        client.succeed("resolvectl dns eth1 | grep -q '10.0.5.3'")
-
-      with subtest("reservations are given to recognized hosts"):
-        reserved.wait_for_unit("systemd-networkd-wait-online.service")
-        reserved.wait_until_succeeds("ip addr show eth1 | grep -q '10.0.5.68/24'")
-    '';
+      networking = {
+        useNetworkd = true;
+        useDHCP = false;
+        interfaces.eth1 = {
+          useDHCP = true;
+          macAddress = "52:54:00:12:01:02";
+        };
+      };
+    };
   };
+
+  testScript = ''
+    import json
+
+    start_all()
+
+    server.wait_for_unit("kea-dhcp4-server.service")
+    client.wait_for_unit("systemd-networkd-wait-online.service")
+
+    with subtest("correct client IP is assigned"):
+      client.wait_until_succeeds("ip addr show eth1 | grep -q '10.0.5.22/24'")
+
+    with subtest("default gateway is assigned"):
+      routes = json.loads(client.succeed("ip --json route"))
+      gateways = {
+        route["gateway"] for route in routes
+        if (
+          route["dev"] == "eth1" and
+          route["protocol"] == "dhcp" and
+          "gateway" in route
+        )
+      }
+
+      assert "10.0.5.3" in gateways, f"Gateway was not assigned: {gateways}"
+
+    with subtest("expected DNS servers are provided"):
+      client.succeed("resolvectl dns eth1 | grep -q '10.0.5.3'")
+
+    with subtest("reservations are given to recognized hosts"):
+      reserved.wait_for_unit("systemd-networkd-wait-online.service")
+      reserved.wait_until_succeeds("ip addr show eth1 | grep -q '10.0.5.68/24'")
+  '';
 }
--- nixos/tests/dns.nix
+++ nixos/tests/dns.nix
@@ -1,101 +1,99 @@
 { makeTest, ... }:
 
-{
-  lookup = makeTest {
-    name = "dns-lookup";
+makeTest {
+  name = "dns-lookup";
 
-    nodes.machine =
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
-      {
-        environment.systemPackages = [
-          pkgs.dig
-          pkgs.doggo
-        ];
+  nodes.machine =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      environment.systemPackages = [
+        pkgs.dig
+        pkgs.doggo
+      ];
 
-        lab.services.dns = {
-          enable = true;
-          interfaces = [ "eth1" ];
-          server.id = "magic-string-nsid";
+      lab.services.dns = {
+        enable = true;
+        interfaces = [ "eth1" ];
+        server.id = "magic-string-nsid";
 
-          zone = {
-            name = "example.com";
-            records = [
-              {
-                type = "TXT";
-                name = "custom-record";
-                value = "magic-string-record";
-              }
-            ];
-          };
-
-          hosts.file = pkgs.writeText "hosts" ''
-            127.1.2.3  custom-host.arpa
-          '';
-
-          # These are not used, but it is important to test the generation
-          # code paths to validate that at least the server does not crash.
-          forward = [
+        zone = {
+          name = "example.com";
+          records = [
             {
-              zone = "non.existent.domain.one";
-              method = "udp";
-              udp.ip = "9.9.9.9";
-            }
-            {
-              zone = "non.existent.domain.two";
-              method = "resolv.conf";
-            }
-            {
-              zone = ".";
-              method = "tls";
-              tls = {
-                ip = "1.1.1.1";
-                servername = "cloudflare-dns.com";
-              };
+              type = "TXT";
+              name = "custom-record";
+              value = "magic-string-record";
             }
           ];
         };
 
-        assertions = [
+        hosts.file = pkgs.writeText "hosts" ''
+          127.1.2.3  custom-host.arpa
+        '';
+
+        # These are not used, but it is important to test the generation
+        # code paths to validate that at least the server does not crash.
+        forward = [
           {
-            assertion = lib.any (port: port == 53) config.networking.firewall.interfaces.eth1.allowedUDPPorts;
-            message = ''
-              DNS server did not open firewall.
-            '';
+            zone = "non.existent.domain.one";
+            method = "udp";
+            udp.ip = "9.9.9.9";
+          }
+          {
+            zone = "non.existent.domain.two";
+            method = "resolv.conf";
+          }
+          {
+            zone = ".";
+            method = "tls";
+            tls = {
+              ip = "1.1.1.1";
+              servername = "cloudflare-dns.com";
+            };
           }
         ];
       };
 
-    testScript = ''
-      start_all()
-      machine.wait_for_unit("coredns.service")
+      assertions = [
+        {
+          assertion = lib.any (port: port == 53) config.networking.firewall.interfaces.eth1.allowedUDPPorts;
+          message = ''
+            DNS server did not open firewall.
+          '';
+        }
+      ];
+    };
 
-      with subtest("NSID is advertised in responses"):
-        nsid_line = machine.succeed("dig +nsid @localhost localhost | grep NSID")
-        print(nsid_line)
-        assert "magic-string-nsid" in nsid_line, "NSID not in response"
+  testScript = ''
+    start_all()
+    machine.wait_for_unit("coredns.service")
 
-      with subtest("resolves custom records"):
-        result = machine.succeed("doggo @localhost TXT custom-record.example.com")
-        print(result)
-        assert "magic-string-record" in result, "Custom record not in response"
+    with subtest("NSID is advertised in responses"):
+      nsid_line = machine.succeed("dig +nsid @localhost localhost | grep NSID")
+      print(nsid_line)
+      assert "magic-string-nsid" in nsid_line, "NSID not in response"
 
-      with subtest("uses local server as system DNS resolver"):
-        # Not specifying the server address - pull from `/etc/resolv.conf`.
-        result = machine.succeed("doggo TXT custom-record.example.com")
-        print(result)
-        assert "magic-string-record" in result, "Local server was not used"
+    with subtest("resolves custom records"):
+      result = machine.succeed("doggo @localhost TXT custom-record.example.com")
+      print(result)
+      assert "magic-string-record" in result, "Custom record not in response"
 
-      with subtest("serves records from the host file"):
-        result = machine.succeed("doggo custom-host.arpa")
-        print(result)
-        assert "127.1.2.3" in result, "Record from host file was not found"
+    with subtest("uses local server as system DNS resolver"):
+      # Not specifying the server address - pull from `/etc/resolv.conf`.
+      result = machine.succeed("doggo TXT custom-record.example.com")
+      print(result)
+      assert "magic-string-record" in result, "Local server was not used"
 
-      # No tests for DNS forwarding. Just try not to break it :)
-    '';
-  };
+    with subtest("serves records from the host file"):
+      result = machine.succeed("doggo custom-host.arpa")
+      print(result)
+      assert "127.1.2.3" in result, "Record from host file was not found"
+
+    # No tests for DNS forwarding. Just try not to break it :)
+  '';
 }
