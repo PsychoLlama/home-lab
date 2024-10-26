2e327ad268b8346bfb997622097eb267eab6400c
Jesse Gibson <JesseTheGibson@gmail.com>
2024-10-26 15:07:13 -0400

    Prove out multi-tenancy with nspawn/nixos
    This works. I've been trying to prove the concept for a long time, but
    never got it working. The goal is for each container to have a private
    network and listen on DHCP for a unique address. Just about all services
    would live in these containers.
    
    That buys me a few things: no conflicts between services (ports, files),
    stronger security (can't bypass auth proxies by hitting localhost),
    multiple instances of the same `services.*`, and keeping a traditional
    maintenance interface (ssh, NixOS modules).
    
    The caveat with macvlans (and the reason it took me so long to discover
    a working implementation) is it doesn't work over wifi. The containers
    have to be connected over ethernet. And all my tests were from my
    laptop...

--- hosts/rpi4-003.nix
+++ hosts/rpi4-003.nix
@@ -1,4 +1,57 @@
+{ pkgs, config, ... }:
+
 {
   home-manager.users.root.home.stateVersion = "23.11";
   system.stateVersion = "21.05";
+  containers.test1 = {
+    autoStart = true;
+    privateNetwork = true;
+    macvlans = [ "end0" ];
+    config = {
+      nixpkgs.pkgs = pkgs;
+      system.stateVersion = config.system.stateVersion;
+
+      networking = {
+        interfaces."mv-end0".useDHCP = true;
+        hostName = "container";
+        firewall.allowedTCPPorts = [ 4444 ];
+      };
+
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+      };
+
+      users.users.root.openssh.authorizedKeys.keyFiles = [
+        ../lib/keys/admin.pub
+        ../lib/keys/deploy.pub
+      ];
+    };
+  };
+
+  containers.test2 = {
+    autoStart = true;
+    privateNetwork = true;
+    macvlans = [ "end0" ];
+    config = {
+      nixpkgs.pkgs = pkgs;
+      system.stateVersion = config.system.stateVersion;
+
+      networking = {
+        interfaces."mv-end0".useDHCP = true;
+        hostName = "container";
+        firewall.allowedTCPPorts = [ 4444 ];
+      };
+
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+      };
+
+      users.users.root.openssh.authorizedKeys.keyFiles = [
+        ../lib/keys/admin.pub
+        ../lib/keys/deploy.pub
+      ];
+    };
+  };
 }
