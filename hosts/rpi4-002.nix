aa94a62efae056dd6f95fa1227a2675dc79b796d
Jesse Gibson <JesseTheGibson@gmail.com>
2024-11-23 14:22:51 -0500

    Allow hosts and containers to communicate
    This is a proof of concept. I had it working where the host and the
    container were able to communicate. This was a surprising catch in the
    first implementation. Apparently macvlans can't communicate with their
    host by default, only other macvlans, so by moving host networking to
    a macvlan as well I lower entry to all containers.
    
    Note that this completely messed up SSH. It fails unless you remove the
    routing tables for the host network. Having two routes is bad news.
    
    Next steps: Add support for associating IP addresses to DHCP client IDs
    instead of mac addresses. Then move all (most?) machines to use macvlans
    as their primary mode of communication. Then create a module for
    constructing containers with macvlans.

--- hosts/rpi4-002.nix
+++ hosts/rpi4-002.nix
@@ -1,4 +1,51 @@
 {
+  config,
+  pkgs,
+  ...
+}:
+
+{
   home-manager.users.root.home.stateVersion = "23.11";
   system.stateVersion = "21.11";
+
+  networking = {
+    # Create bridge using physical interface
+    macvlans.mv0 = {
+      mode = "bridge";
+      interface = "end0";
+    };
+
+    interfaces.mv0.useDHCP = true;
+    firewall.allowedTCPPorts = [ 4444 ];
+    # Bridge itself can use DHCP for host
+    # interfaces.br0.useDHCP = true;
+  };
+
+  containers.test1 = {
+    autoStart = true;
+    privateNetwork = true;
+    macvlans = [ "end0:bridge" ];
+
+    config = {
+      nixpkgs.pkgs = pkgs;
+      system.stateVersion = config.system.stateVersion;
+
+      networking = {
+        # Container interface will get its own IP
+        interfaces.bridge.useDHCP = true;
+        firewall.allowedTCPPorts = [ 4444 ];
+        hostName = "bacon";
+      };
+
+      services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+      };
+
+      users.users.root.openssh.authorizedKeys.keyFiles = [
+        ../lib/keys/admin.pub
+        ../lib/keys/deploy.pub
+      ];
+    };
+  };
 }
