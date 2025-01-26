195dd9ce35948043568286d85bafa702819ee624
Jesse Gibson <JesseTheGibson@gmail.com>
2025-01-26 17:14:55 -0500

    Expose VPN through a CF tunnel
    This is an experiment. I want to share my VPN with a fellow home-lab
    operator with restricted ACLs to shared resources. Having a public VPN
    to bootstrap the connection is useful, not only for that case, but
    bootstrapping my own devices.
    
    I'm using Cloudflare for TLS termination. I'm not thrilled that they
    could MITM me, but the risk seems low and the reputational damage to
    them would be high, so I'm not too worried about it. Plus, it would only
    help if they MITM'd me during bootstrap. (I think.)

--- platforms/nixos/modules/lab/profiles/vpn/server.nix
+++ platforms/nixos/modules/lab/profiles/vpn/server.nix
@@ -13,16 +13,45 @@ in
     '';
   };
 
-  config.lab.services.vpn.server = lib.mkIf cfg.enable {
-    enable = true;
+  config = lib.mkIf cfg.enable {
+    lab.services.vpn.server = {
+      enable = true;
 
-    url = "http://${config.networking.hostName}.host.${datacenter}.${domain}:${toString port}";
-    dns.zone = "${datacenter}.vpn.${domain}";
-    openFirewall = true;
+      url = "http://${config.networking.hostName}.host.${datacenter}.${domain}:${toString port}";
+      dns.zone = "${datacenter}.vpn.${domain}";
+      openFirewall = true;
 
-    listen = {
-      address = "0.0.0.0";
-      port = 8080;
+      listen = {
+        address = "0.0.0.0";
+        port = 8080;
+      };
+    };
+
+    # Experimental: Expose the VPN server through a Cloudflare Tunnel.
+    # TODO: Move this to a separate module and route services by VPN/ACL.
+    services.cloudflared = {
+      enable = true;
+
+      # Depends on Cloudflare for TLS termination. This is a security risk,
+      # but considering the reputational damage to Cloudflare if they MITM'd
+      # it, it's low on my list of concerns.
+      #
+      # NOTE: The default certificate only works for immediate subdomains.
+      tunnels.vpn = {
+        credentialsFile = config.age.secrets.vpn-tunnel-key.path;
+        default = "http_status:404";
+        ingress = {
+          "vpn.${domain}" = {
+            service = "http://localhost:${toString port}";
+          };
+        };
+      };
+    };
+
+    age.secrets.vpn-tunnel-key = {
+      file = ./vpn-tunnel-key.age;
+      group = config.services.cloudflared.group;
+      owner = config.services.cloudflared.user;
     };
   };
 }
--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,22 @@
 let
   inherit (flake.inputs.nixpkgs) lib;
   flake = builtins.getFlake (toString ./.);
-  hostKeys = lib.mapAttrs (_: node: node.config.lab.host.publicKeys) flake.outputs.colmenaHive.nodes;
+
+  /**
+    Find all public keys for nodes that match the given predicate.
+  */
+  getPublicKeysWhere =
+    predicate:
+    lib.pipe flake.outputs.colmenaHive.nodes [
+      (lib.filterAttrs (_: node: predicate node))
+      (lib.mapAttrsToList (_: node: node.config.lab.host.publicKeys))
+      (lib.flatten)
+    ];
 in
 
 {
-  # Placeholder. No keys defined yet.
-  "example.age".publicKeys = hostKeys.rpi4-003;
+  # Generated with: `cloudflared tunnel create <name>`
+  "vpn-tunnel-key.age".publicKeys = getPublicKeysWhere (
+    node: node.config.lab.profiles.vpn.server.enable
+  );
 }
