fbc0a26fd976cb8bb619f86d4bcc8f4c22a0282f
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-21 19:51:40 -0500

    Expose Grafana on all interfaces for Tailscale access

--- platforms/nixos/modules/lab/stacks/observability/default.nix
+++ platforms/nixos/modules/lab/stacks/observability/default.nix
@@ -81,6 +81,11 @@ in
       grafana = {
         enable = true;
 
+        settings.server = {
+          http_addr = "0.0.0.0";
+          http_port = 3000;
+        };
+
         provision = {
           enable = true;
           datasources.settings.datasources = [
